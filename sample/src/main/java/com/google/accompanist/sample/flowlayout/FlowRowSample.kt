1f72cb3b9a915c752021b90896eb14aec8119205
Lee <leesunghoon413@gmail.com>
2022-04-21 17:24:59 -0400

    Update FlowRowSample.kt

--- sample/src/main/java/com/google/accompanist/sample/flowlayout/FlowRowSample.kt
+++ sample/src/main/java/com/google/accompanist/sample/flowlayout/FlowRowSample.kt
@@ -19,15 +19,26 @@ package com.google.accompanist.sample.flowlayout
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.google.accompanist.flowlayout.FlowRow
 import com.google.accompanist.sample.AccompanistSampleTheme
 import com.google.accompanist.sample.R
+import kotlin.random.Random
 
 class FlowRowSample : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,12 +51,41 @@ class FlowRowSample : ComponentActivity() {
                             title = { Text(text = stringResource(R.string.flowlayout_title_row)) }
                         )
                     }
-                ) { padding ->
-                    FlowRow(Modifier.padding(padding)) {
-                        SampleContent()
+                ) { _ ->
+                    val scrollState = rememberScrollState()
+                    FlowRow(
+                        Modifier
+                            .padding(all = 20.dp)
+                            .verticalScroll(scrollState)) {
+                        FlowRowSampleContent()
                     }
                 }
             }
+        }
+    }
+}
+
+@Composable
+internal fun FlowRowSampleContent() {
+
+    val MIN_WIDTH = 80
+    val MAX_WIDTH = 300
+
+    repeat(130) { number ->
+
+        val randomWidth = Random(System.currentTimeMillis()).nextInt(MAX_WIDTH-MIN_WIDTH) + MIN_WIDTH
+
+        Box(
+            modifier = Modifier
+                .size(
+                    width = randomWidth.dp,
+                    height = 50.dp
+                )
+                .background(Color.LightGray)
+                .border(2.dp, Color.DarkGray),
+            contentAlignment = Alignment.Center,
+        ) {
+            Text(text = number.toString())
         }
     }
 }
